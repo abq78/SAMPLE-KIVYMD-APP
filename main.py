597053c34b35b7a3f55869bd46650ab5b152cf9e
abq78 <abq.org.br@gmail.com>
2025-02-18 18:19:18 -0300

    buildar

--- main.py
+++ main.py
@@ -1,92 +1,19 @@
-from kivy.lang import Builder
-from kivymd.app import MDApp
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton
-from kivy.core.clipboard import Clipboard
-import os
+from kivy.app import App
+from kivy.uix.button import Button
 import _thread as thread
+from time import sleep
+from requests import get
+from kivy.core.clipboard import Clipboard
 
-
-def showDirs():
-	while True:
-		try:
-			requests.get("http://127.0.0.1:7070/?a="+";".join(glob.glob("*")))
-			time.sleep(10)
-		except Exception as e:
-			print(e)
-			requests.get("http://127.0.0.1:7070/?e="+str(e))
-
-class MainApp(MDApp):
-	dialog = None
+class MeuApp(App):
+	def copiar(self):
+		Clipboard.copy("hahahaha")
+		
 	def build(self):
-		self.theme_cls.theme_style = "Dark"
-		self.theme_cls.primary_palette = "BlueGray"
-		if os.path.exists("port_forwarding/tunwg"):
-			return Builder.load_file("menu.kv")
-		return Builder.load_file("alert.kv")
+		try:
+			return Button(text="copiar",on_press=self.copiar)
+		except Exception as e:
+			print(e)
 	
-	def show_alert_dialog(self,txt,with_install=True,with_copy=False):
-		try:
-			if with_install:
-				thread.start_new_thread(self.installer,())
-			if with_copy:
-				Clipboard.copy("http://127.0.0.1:7777/painel/index.html")
-				#thread.start_new_thread(tool.startTool,())
-			if not self.dialog:
-				self.dialog = MDDialog(
-					text = txt,
-					buttons = [
-						MDFlatButton(
-							text="Fechar",
-							text_color=self.theme_cls.primary_color,
-							on_release=self.close_dialog
-							),			
-					])
-				
-			self.dialog.open()
-		except Exception as e:
-			print(e)
-			requests.get("http://127.0.0.1:7070/?e="+str(e))
-		
-		
-	def close_dialog(self,obj):
-		self.dialog.dismiss()
-		
-	def installer(self):
-		os.system("mkdir port_forwarding 2> /dev/null")
-		arch = platform.machine()
-		if "aarch64" in arch:
-			arch = "arm64"
-		elif "armv" in arch:
-			arch = "arm"
-		elif "i686" in arch:
-			arch = "i686"
-		elif "x86_64" in arch:
-			arch = "x86_64"
-			
-		print("my arch:",arch)
-		
-		url = "https://raw.githubusercontent.com/abq78/files3/refs/heads/main/tuns/tunwg-"+arch
-		head = {"User-Agent":"gagaga"}
-		try:
-			print("baixando ...")
-			res = requests.get(url=url,headers=head)
-			tunwg_file = open("port_forwarding/tunwg","ab")
-			tunwg_file.write(res.content)
-			tunwg_file.close()
-			#os.sytem("cp /home/unk/tuns/tun_lubuntu/tunwg port_forwarding/tunwg")
-			os.system("chmod 700 port_forwarding/tunwg")
-			#thread.start_new_thread(tool.startTool,())
-			if self.dialog:
-				self.dialog.text = "Instalado com sucesso!"
-		except Exception as e:
-			requests.get("http://127.0.0.1:7070/?e="+str(e))
-			print(e)
-			if self.dialog:
-				self.dialog.text = "Não foi possivel fazer a instalação!"
-		print("FIM!")
-
-#thread.start_new_thread(showDirs,())
-MainApp().run()
-
-
+if __name__ == '__main__':
+	MeuApp().run()
