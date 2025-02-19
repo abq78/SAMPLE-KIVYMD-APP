d9d0b22adf2144a5655d5fc7d08d5adfe1ca33eb
abq78 <abq.org.br@gmail.com>
2025-02-18 22:52:54 -0300

    buildar

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from kivy.app import App
 from kivy.uix.button import Button
 import _thread as thread
 from time import sleep
-from requests import get
+import requests
 from kivy.core.clipboard import Clipboard
+import tool
 
 class MeuApp(App):
-  def copiar(self):
+  def copiar(self,obj):
     Clipboard.copy("http://127.0.0.1:7777/painel/index.html")
     return False
+ 
+  def rodar(self,obj):
+  	thread.start_new_thread(tool.startTool,())
+ 
   def build(self):
     try:
-      return Button(text="Clique aqui para copiar (http://127.0.0.1:7777/painel/index.html)",on_press=self.copiar)
+      return Button(text="Clique aqui para copiar (http://127.0.0.1:7777/painel/index.html)",on_press=self.rodar)
     except:
       None
 
